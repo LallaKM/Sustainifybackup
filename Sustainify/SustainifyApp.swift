abe1d84cea565ee6fe068a7c254b1b3baf98c271
bhullarravia <bhullarravia@gmail.com>
2024-06-24 20:14:43 -0400

    Initial Commit

---
+++ Sustainify/SustainifyApp.swift
@@ -0,0 +1,17 @@
+//
+//  SustainifyApp.swift
+//  Sustainify
+//
+//  Created by Ravia Bhullar on 6/24/24.
+//
+
+import SwiftUI
+
+@main
+struct SustainifyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
